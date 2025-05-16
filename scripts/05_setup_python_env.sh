75e2e047655fe1299f1c089344c1c4db33066b09
deianp189 <deianp189>
2025-05-16 21:51:33 +0000

    nueva libreria

--- scripts/05_setup_python_env.sh
+++ scripts/05_setup_python_env.sh
@@ -6,4 +6,4 @@ apt-get install -y python3-venv python3-pip
 python3 -m venv "$VENV"
 
 "$VENV/bin/pip" install --upgrade pip
-"$VENV/bin/pip" install fastapi 'uvicorn[standard]' psutil PyMySQL
+"$VENV/bin/pip" install fastapi 'uvicorn[standard]' psutil PyMySQL requests
